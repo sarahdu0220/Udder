f877303f8a2a8cc0d93991ae65f55bc7d192d72f
Sarah Du <sarahdu0220@gmail.com>
2024-08-05 16:45:15 -0400

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Shuang Du on 2024-08-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Shuang Du on 2024-08-05.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
